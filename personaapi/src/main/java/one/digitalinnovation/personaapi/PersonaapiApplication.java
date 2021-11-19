191906ec3746760fcce00843c8e529ce2449eef0
eduardomoisesqa <eduardo.silva.cps@elo.com.br>
2021-11-19 09:22:35 -0300

    first commit

---
+++ personaapi/src/main/java/one/digitalinnovation/personaapi/PersonaapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaapiApplication.class, args);
+	}
+
+}
---
+++ personaapi/src/test/java/one/digitalinnovation/personaapi/PersonaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
